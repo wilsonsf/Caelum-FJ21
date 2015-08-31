f1e0434dc709a5d6d931feadb45819dbc56db4b5
Wilson de Farias <wilsonsf1@gmail.com>
2015-08-31 12:59:21 -0300

    2.11-2 Criada classe ContatoDao

---
+++ fj21-jdbc/src/br/com/caelum/jdbc/dao/ContatoDao.java
@@ -0,0 +1,39 @@
+package br.com.caelum.jdbc.dao;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import br.com.caelum.jdbc.ConnectionFactory;
+import br.com.caelum.modelo.Contato;
+
+public class ContatoDao {
+	private Connection connection;
+	
+	public ContatoDao() {
+		this.connection = new ConnectionFactory().getConnection();
+	}
+	
+	public void adiciona(Contato contato) {
+		String sql = "INSERT INTO contatos " +
+					"(nome,email,endereco,dataNascimento) " +
+					"VALUES (?,?,?,?)";
+		
+		try {
+			PreparedStatement stmt = this.connection.prepareStatement(sql);
+			
+			stmt.setString(1, contato.getNome());
+			stmt.setString(1, contato.getEmail());
+			stmt.setString(2, contato.getEndereco());
+			stmt.setDate(4, new Date(contato.getDataNascimento().getTimeInMillis()));
+			
+			stmt.execute();
+			stmt.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
+}
