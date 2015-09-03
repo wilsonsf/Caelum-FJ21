718a3ee396ff8ff215c1462979a47b7960489336
Wilson de Farias <wilsonsf1@gmail.com>
2015-09-03 12:28:01 -0300

    2.17-1 Implementação da lógica de altera(Contato) no ContatoDao

--- fj21-agenda/src/br/com/caelum/agenda/dao/ContatoDao.java
+++ fj21-agenda/src/br/com/caelum/agenda/dao/ContatoDao.java
@@ -82,6 +82,29 @@ public class ContatoDao {
 		}
 	}
 
+	public void altera(Contato contato) {
+		String sql = "UPDATE contatos SET nome=?, email=?, endereco=?, dataNascimento=? where id=?";
+
+		try {
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.setString(1, contato.getNome());
+			stmt.setString(2, contato.getEmail());
+			stmt.setString(3, contato.getEndereco());
+			stmt.setDate(4, new Date(contato.getDataNascimento()
+					.getTimeInMillis()));
+			stmt.setLong(5, contato.getId());
+
+			stmt.execute();
+			stmt.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void remove(ContatoDao dao) {
+
+	}
+
 	private Contato montaContatoComResultSet(ResultSet rs) {
 		try {
 			Contato contato = new Contato();
@@ -101,4 +124,5 @@ public class ContatoDao {
 			throw new DAOException(e);
 		}
 	}
+
 }
