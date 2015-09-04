3404b4cf8a88c70ad831c3e84003a3333926c9d1
Wilson de Farias <wilsonsf1@gmail.com>
2015-09-04 13:57:44 -0300

    12.6 Adicionada lógica para verificar a existência do usuário

--- fj21-tarefas/src/br/com/caelum/tarefas/controller/LoginController.java
+++ fj21-tarefas/src/br/com/caelum/tarefas/controller/LoginController.java
@@ -1,7 +1,12 @@
 package br.com.caelum.tarefas.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.caelum.tarefas.dao.JdbcUsuarioDao;
+import br.com.caelum.tarefas.modelo.Usuario;
 
 @Controller
 public class LoginController {
@@ -11,4 +16,12 @@ public class LoginController {
 		return "formulario-login";
 	}
 	
+	@RequestMapping("efetuaLogin")
+	public String efetuaLogin(Usuario usuario, HttpSession session) {
+		if (new JdbcUsuarioDao().existeUsuario(usuario)) {
+			session.setAttribute("usuarioLogado", usuario);
+			return "menu";
+		}
+		return "redirect:loginForm";
+	}
 }
