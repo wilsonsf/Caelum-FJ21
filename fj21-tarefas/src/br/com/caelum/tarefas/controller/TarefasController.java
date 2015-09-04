5b66a416f9947c3f8974d99a6863263d5ec63591
Wilson de Farias <wilsonsf1@gmail.com>
2015-09-04 12:50:40 -0300

    11.21-4 Adicionada lógica finalizaTarefa no TarefasController

--- fj21-tarefas/src/br/com/caelum/tarefas/controller/TarefasController.java
+++ fj21-tarefas/src/br/com/caelum/tarefas/controller/TarefasController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.com.caelum.tarefas.dao.JdbcTarefaDao;
 import br.com.caelum.tarefas.modelo.Tarefa;
@@ -60,5 +61,12 @@ public class TarefasController {
 		JdbcTarefaDao dao = new JdbcTarefaDao();
 		dao.altera(tarefa);
 		return "redirect:listaTarefas";
+	}
+	
+	@ResponseBody
+	@RequestMapping("finalizaTarefa")
+	public void finaliza(Long id){
+		JdbcTarefaDao dao = new JdbcTarefaDao();
+		dao.finaliza(id);
 	}
 }
